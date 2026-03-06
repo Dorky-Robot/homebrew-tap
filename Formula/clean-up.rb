4825ed7dfb6a5e1031391f161e3569f6a5b4094e
Felix Flores <felix@dorkyrobot.com>
2026-03-06 10:39:16 -0500

    Update clean-up to v1.1.0

--- Formula/clean-up.rb
+++ Formula/clean-up.rb
@@ -1,8 +1,8 @@
 class CleanUp < Formula
   desc "Interactive CLI to reclaim disk space from project build artifacts and caches"
   homepage "https://github.com/Dorky-Robot/clean-up"
-  url "https://github.com/Dorky-Robot/clean-up/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "8cd3f781850f27d59bf70568d88a03a2a3c58bcb03b35647098845694d4efa15"
+  url "https://github.com/Dorky-Robot/clean-up/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "bc92debde27555342506ff90fe9affb3e5e34a2b5f9d0b0610d3ea1c3eaf56dc"
   license "MIT"
 
   def install
