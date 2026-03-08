9d8a3ce1efa9be238651fb8d672d614598b1b73c
Felix Flores <felix@dorkyrobot.com>
2026-03-08 01:48:01 -0500

    tunnels 0.5.1: R (sync CF) works from both tabs

--- Formula/tunnels.rb
+++ Formula/tunnels.rb
@@ -1,16 +1,16 @@
 class Tunnels < Formula
   desc "k9s-style TUI for managing cloudflared tunnels and local services"
   homepage "https://github.com/Dorky-Robot/tunnels"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.5.0/tunnels-v0.5.0-aarch64-apple-darwin.tar.gz"
-      sha256 "79673d11ca590ca5d0cdce89b5df7a17cb0ee820026811b0e665116ad42b1eb3"
+      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.5.1/tunnels-v0.5.1-aarch64-apple-darwin.tar.gz"
+      sha256 "542c56025a5847ef264a0985dfd3a6610ef4a47873eb973f67ebe9cdb4dec5bb"
     elsif Hardware::CPU.intel?
-      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.5.0/tunnels-v0.5.0-x86_64-apple-darwin.tar.gz"
-      sha256 "73d38549e897d64751d4b1a6bee56d46520a890bd6a5de91a92b852dc60aaeca"
+      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.5.1/tunnels-v0.5.1-x86_64-apple-darwin.tar.gz"
+      sha256 "7c5da88fee816604443d7474f4bad37ee1d554161a4c1988ad3d6d35e01622e1"
     end
   end
 
