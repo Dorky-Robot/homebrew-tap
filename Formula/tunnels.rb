7bdbdee1bfad22763ea606333ddb9e893695a8c8
Felix Flores <felix@dorkyrobot.com>
2026-03-07 22:43:03 -0500

    tunnels v0.3.0: import + migrate

--- Formula/tunnels.rb
+++ Formula/tunnels.rb
@@ -1,13 +1,13 @@
 class Tunnels < Formula
   desc "k9s-style TUI for managing multiple cloudflared tunnel instances"
   homepage "https://github.com/Dorky-Robot/tunnels"
-  version "0.2.1"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.2.1/tunnels-v0.2.1-aarch64-apple-darwin.tar.gz"
-      sha256 "058dfcc3865038e85aef6d5d078b47dda8480dedffe93f72585286dd0b2e6dff"
+      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.3.0/tunnels-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "5a86d257a447d39d2ded6405dfde3064540cb68a3b2874b3a176b35b2577a14e"
     end
   end
 
