91a8c32bcf36921b2a387d99efed950559cd7879
Felix Flores <felix@dorkyrobot.com>
2026-03-07 22:27:09 -0500

    tunnels v0.2.0: no sudo, user-level LaunchAgents

--- Formula/tunnels.rb
+++ Formula/tunnels.rb
@@ -1,13 +1,13 @@
 class Tunnels < Formula
   desc "k9s-style TUI for managing multiple cloudflared tunnel instances"
   homepage "https://github.com/Dorky-Robot/tunnels"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.1.0/tunnels-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "bb0c9fe371e246763b9be64f9bfda2a67f332111df66502f98d2cb7a15d83c9a"
+      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.2.0/tunnels-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "e537482bb372ea8f6902fefc9f4d63d87ea80d2d8d8bcaad7bb2fb351ec2dd75"
     end
   end
 
