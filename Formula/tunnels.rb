9c4068e876bcc60ef47f17a0d6c76fe7cab37893
Felix Flores <felix@dorkyrobot.com>
2026-03-08 03:08:31 -0400

    tunnels 0.5.3: multi-account CF ingress resolution

--- Formula/tunnels.rb
+++ Formula/tunnels.rb
@@ -1,16 +1,16 @@
 class Tunnels < Formula
   desc "k9s-style TUI for managing cloudflared tunnels and local services"
   homepage "https://github.com/Dorky-Robot/tunnels"
-  version "0.5.2"
+  version "0.5.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.5.2/tunnels-v0.5.2-aarch64-apple-darwin.tar.gz"
-      sha256 "ed9eeb53858940e17a10222213076a3224838d0e2e7072ff2e230c82b5e1d2ff"
+      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.5.3/tunnels-v0.5.3-aarch64-apple-darwin.tar.gz"
+      sha256 "1da09534e0c295a0def9a031dc08482dee48579e79e0e0befb1adbbbc223a513"
     elsif Hardware::CPU.intel?
-      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.5.2/tunnels-v0.5.2-x86_64-apple-darwin.tar.gz"
-      sha256 "00b9aa481e45fb639c2e3aed79e7c7f2f77445c30e3904126096c6ef9835c74c"
+      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.5.3/tunnels-v0.5.3-x86_64-apple-darwin.tar.gz"
+      sha256 "ac0ec3afe46ac4829383f4cfc433bb68897393403e479bdbf933da50dbce01ad"
     end
   end
 
