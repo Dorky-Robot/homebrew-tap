046d7dbedfb3dc0cc538154e48fcd755ee8d82aa
Felix Flores <felix@dorkyrobot.com>
2026-03-09 17:13:05 -0400

    tunnels 0.8.1

--- Formula/tunnels.rb
+++ Formula/tunnels.rb
@@ -1,16 +1,16 @@
 class Tunnels < Formula
   desc "k9s-style TUI for managing cloudflared tunnels and local services"
   homepage "https://github.com/Dorky-Robot/tunnels"
-  version "0.8.0"
+  version "0.8.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.8.0/tunnels-v0.8.0-aarch64-apple-darwin.tar.gz"
-      sha256 "e1f45f0659e742798cca823cd94f7d4c33dbfb6be8c9d27759cb7139426a33a5"
+      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.8.1/tunnels-v0.8.1-aarch64-apple-darwin.tar.gz"
+      sha256 "db822518b416c964692137067b5976d1349da84fb28319860d26f80f6f142519"
     elsif Hardware::CPU.intel?
-      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.8.0/tunnels-v0.8.0-x86_64-apple-darwin.tar.gz"
-      sha256 "197671ba67eb19e951fff3564b7400fc2e3e10703fafde1eaf0e931b73ec6c98"
+      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.8.1/tunnels-v0.8.1-x86_64-apple-darwin.tar.gz"
+      sha256 "f21780702b147455f154e54264f8dfddbc2d14698278f62830fd279e39cc3e25"
     end
   end
 
