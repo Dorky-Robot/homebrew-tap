e2bd1e67986fb3ad5fd4ba76d0b14fddbaab87ef
Felix Flores <felix@dorkyrobot.com>
2026-03-12 03:30:26 -0400

    Update yelo to v0.1.1

--- Formula/yelo.rb
+++ Formula/yelo.rb
@@ -1,18 +1,19 @@
 class Yelo < Formula
   desc "FTP-style CLI and TUI for Amazon S3 and Glacier"
   homepage "https://github.com/Dorky-Robot/yelo"
-  url "https://github.com/Dorky-Robot/yelo/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "8acab7bf869b42e38c7e4413bbb9a1a8c8b423b1e7d284dc7659dba9cfffc28c"
+  url "https://github.com/Dorky-Robot/yelo/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "873e6e86bbfdaeb576b40f31335e86a1b1c8d20a5aac665298328ce912f76a12"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "."
+    ldflags = "-s -w -X github.com/dorkyrobot/yelo/cmd.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "."
     man1.install "doc/yelo.1"
   end
 
   test do
-    system bin/"yelo", "help"
+    assert_match "yelo #{version}", shell_output("#{bin}/yelo -v")
   end
 end
