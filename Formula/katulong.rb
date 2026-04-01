2c725131088ff533d794582ffe678047da7a919b
Felix Flores <felixflores@users.noreply.github.com>
2026-04-01 14:06:10 +0000

    katulong 0.44.40 (fix URL)

--- Formula/katulong.rb
+++ Formula/katulong.rb
@@ -1,7 +1,7 @@
 class Katulong < Formula
   desc "Self-hosted web terminal with tmux sessions and WebAuthn security"
   homepage "https://github.com/Dorky-Robot/katulong"
-  url "https://github.com/Dorky-Robot/katulong/archive/refs/tags/v0.44.38.tar.gz"
+  url "https://github.com/Dorky-Robot/katulong/archive/refs/tags/v0.44.40.tar.gz"
   sha256 "fd206c10b8f84fda172e3c65c18385b56588c170d304264d1195a98e08ba6ffc"
   license "MIT"
 
