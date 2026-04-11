8a293a8077102aaf1cab63ea580c11e44cb762ce
Felix Flores <holla@felixflor.es>
2026-04-10 20:02:19 -0400

    tunnels 0.9.2

--- Formula/tunnels.rb
+++ Formula/tunnels.rb
@@ -1,18 +1,18 @@
 class Tunnels < Formula
   desc "k9s-style TUI for managing cloudflared tunnels and local services"
   homepage "https://github.com/Dorky-Robot/tunnels"
-  version "0.9.1"
+  version "0.9.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.9.1/tunnels-v0.9.1-aarch64-apple-darwin.tar.gz"
-      sha256 "4e894bb5d86dae66e2e8a382e990a84035f84130ecd3f172910fc9dd822837f5"
+      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.9.2/tunnels-v0.9.2-aarch64-apple-darwin.tar.gz"
+      sha256 "9edc5b4eb4865250fd6c7e3e23cd8fa58651c68ddddf03ef8e50c4c83fa393da"
     end
 
     on_intel do
-      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.9.1/tunnels-v0.9.1-x86_64-apple-darwin.tar.gz"
-      sha256 "988c39fdc9f6ed1f18ceb4758e1905128b6e1b7b224cda8d4eb209035ffd963a"
+      url "https://github.com/Dorky-Robot/tunnels/releases/download/v0.9.2/tunnels-v0.9.2-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_NEEDS_INTEL_BUILD"
     end
   end
 
