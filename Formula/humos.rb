5b8dfd422514b5a382044b64a95797617140bfc4
Felix Flores <holla@felixflor.es>
2026-04-23 10:50:18 -0400

    chore: relicense as MIT OR Apache-2.0 dual license

---
+++ Formula/humos.rb
@@ -0,0 +1,38 @@
+class Humos < Formula
+  desc "Personal OS: sovereign binaries that own your data via filesystem contracts"
+  homepage "https://github.com/Dorky-Robot/humOS"
+  version "0.1.0"
+  license any_of: ["MIT", "Apache-2.0"]
+
+  on_macos do
+    on_arm do
+      url "https://github.com/Dorky-Robot/humOS/releases/download/v0.1.0/humos-v0.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "48d218d5e55eb622395ce405431a9bca8cec57b022c4dee96cb4973ccfe24fc4"
+    end
+  end
+
+  depends_on "isync"
+  depends_on "himalaya"
+
+  def install
+    bin.install "humos", "humos-mail", "humos-web"
+  end
+
+  def caveats
+    <<~EOS
+      humOS state lives in ~/.humOS/ (created on first use).
+
+      Quick start:
+        echo -n "$PASSWORD" | humos mail account add gmail you@gmail.com
+        humos mail fetch
+        humos mail
+
+      Optional: install Ollama (https://ollama.com) to enable `humos mail triage`.
+    EOS
+  end
+
+  test do
+    assert_match "humos", shell_output("#{bin}/humos --help")
+    assert_match "mail", shell_output("#{bin}/humos-mail --help")
+  end
+end
